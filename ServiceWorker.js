b369204ff85859ec00dbd0359f32eb7bc98a2ee6
kkomdori <kkomdori@gmail.com>
2025-12-30 14:02:29 +0900

    Initial commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "kkomdori-Path-Directed WFC simulator-1.0";
+const contentToCache = [
+    "Build/Path-Directed_WFC_WebGL.loader.js",
+    "Build/Path-Directed_WFC_WebGL.framework.js",
+    "Build/Path-Directed_WFC_WebGL.data",
+    "Build/Path-Directed_WFC_WebGL.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
